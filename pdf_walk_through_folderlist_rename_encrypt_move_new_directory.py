3c7ff7f0161027de6f2878742cb73194fd54083e
ReadbytheBeach <Heinekenblue@163.com>
2023-02-19 13:03:45 +0800

    modify pdf_walk_through_folderlist_rename_encrypt_move_new_directory.py

--- pdf_walk_through_folderlist_rename_encrypt_move_new_directory.py
+++ pdf_walk_through_folderlist_rename_encrypt_move_new_directory.py
@@ -1,7 +1,42 @@
+'''
+目的：
+    遍历一个文件夹
+    找到所有pdf文件
+    依次加密
+    生成新文件名以:_encripted.pdf结尾
+    并将文件移到指定路径下
+技巧：
+    路径： Path(r'D:\03_program\python\source_folder')
+    正则表达式分离文件名： re.compile(r'(.*)(.pdf)$')
+    找到的文件生成绝对路径： os.path.join(dir, filename)
+    如何生成一个新pdf文件并加密
+        1.打开一个PDF文件对象: pdf_reader = PyPDF2.PdfFileReader(open(dir_name,'rb'))
+        2.生成一个PDF写入文件 （可理解成缓存文件） : pdf_write = PyPDF2.PdfFileWriter()
+        3.依次将PDF页面存入写入文件: 
+             for pageNum in range(pdf_reader.numPages): # 总的页数,从'0'算起
+                # print(pageNum)
+                pdf_write.addPage (pdf_reader.getPage(pageNum)) # 将原文件每一页写入新文件中
+        4.加密写入文件:  pdf_write.encrypt('xj')
+        5.给新PDF文件取名:  new_pdf_name = pdf_regex.search(filename).group(1) + new_postfix 
+        6.生成新PDF文件: 
+            # 创建新文件名的对象
+            result_pdf  = open(new_pdf_name, 'wb')
+            # 将内容写入新创建的pdf文件
+            pdf_write.write(result_pdf)
+        7.新PDF文件添加完内容后关闭: result_pdf.close()
+    打开新生成文件
+    查看加密状态:  print('Is PDF- %s encripted?  Result is: %s' %(new_pdf_name, re_pdf_reader.isEncrypted)) 
+    创建源文件路径+文件名
+    创建目的地文件路径+文件名
+    移动文件到目的地： shutil.move(current_addr,dest_path)
+    
+'''
+
 import os, PyPDF2
 from pathlib import Path
 import re
 import shutil
+
 
 pdf_path = Path(r'D:\03_program\python\source_folder')
 
