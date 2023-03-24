cf03ab8cbb8ef415b911871b2518a6463805895c
ReadbytheBeach <Heinekenblue@163.com>
2023-03-24 08:33:44 +0800

    add absolute address for visit pdf folder

--- pdf_combination.py
+++ pdf_combination.py
@@ -18,23 +18,26 @@
 import PyPDF2, os
 # get all the pdf files
 pdfFiles = []
-for filename in os.listdir('.'): # '.'表示当前目录下 
+for filename in os.listdir('.\H07063'): # '.'表示当前目录下 
     if filename.endswith('.pdf'): 
         pdfFiles.append(filename)
 pdfFiles.sort(key = str.lower)
 
+directory = 'D:\\03_program\\python\\H07063\\'
+
 pdfWriter = PyPDF2.PdfFileWriter()  # 生成一个pdfWrite的对象
 
+print(pdfFiles)
 # loop throgh all the PDF files
 for filename in pdfFiles:
-    pdfFileObj = open(filename, 'rb')  # 将文档以二进制打开赋值给一个File对象： pdfFileObj
+    pdfFileObj = open(directory+ filename,'rb')  # 将文档以二进制打开赋值给一个File对象： pdfFileObj
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj) # 将pdfFileObj赋值给一个PDFFileReader对象： pdfReader
     # Loop through all the pages (except the 1st page) and add them.
     for pageNam in range(1, pdfReader.numPages):
         pageObj = pdfReader.getPage(pageNam)    # 取出页面
         pdfWriter.addPage(pageObj)  # 页面加入pdfWriter对象
 # save the resulting PDF to a file
-pdfOutput = open('all.pdf','wb')  # 将一个pdf文件赋予二进制写入并生成一个文件对象： pdfOutput
+pdfOutput = open(directory+'all.pdf','wb')  # 将一个pdf文件赋予二进制写入并生成一个文件对象： pdfOutput
 pdfWriter.write(pdfOutput) # 将PDF写入对象： pdfWriter中的内容赋给该文件对象
 pdfOutput.close() # 写完，关闭文件
 
